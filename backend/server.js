e4e94ef06bd5d30f395b53d7f86a6239735e4dcd
letDiyaCode <diyalimbani173@gmail.com>
2025-12-22 05:57:27 +0530

    Serve frontend from backend on npm start

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,11 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
+
 // Global state (in production, use database)
 let cityGraph = null;
 let rideMatcher = null;
